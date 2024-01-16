08766c2d4bdd77508c65d31a6a177e61cb61f5a9
Arthur <arthur@mondon.pro>
2024-01-16 10:04:45 +0100

    create script to detect if hands are over the head

---
+++ PythonProject_save_the_turtles/Tests/hands_detection/detect_hands.py
@@ -0,0 +1,43 @@
+import cv2
+from ultralytics import YOLO
+
+def are_hands_in_air(keypoints):
+    # Récupérer les coordonnées "y" des poignets et des épaules
+    left_hand_y = keypoints[0][9][1]
+    right_hand_y = keypoints[0][10][1]
+    left_eye_y = keypoints[0][1][1]
+    right_eye_y = keypoints[0][2][1]
+
+   # si les mains ne sont pas détectées, on retourne false
+    if left_hand_y == 0 or right_hand_y == 0:
+        return False
+
+    # Si les mains sont plus hautes que yeux, alors les mains sont en l'air
+    return left_hand_y < left_eye_y and right_hand_y < right_eye_y
+
+def start_yolo_hands_detection():
+    model = YOLO("../yolo/yolov8n-pose.pt")
+    cap = cv2.VideoCapture(0)
+
+    while True:
+        success, frame = cap.read()
+
+        if not success:
+            break
+
+        results = model(frame)
+        keypoints = results[0].keypoints.xy.tolist()
+
+        cv2.imshow("Yolo Detection", frame)
+
+        hands_in_air = are_hands_in_air(keypoints)
+        print("Mains en l'air:", hands_in_air)
+
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
+
+    cap.release()
+    cv2.destroyAllWindows()
+
+if __name__ == "__main__":
+    start_yolo_hands_detection()
