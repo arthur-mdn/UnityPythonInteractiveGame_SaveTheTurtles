245f35555514c0a2ddfc0a96f9b49004622a40e8
Arthur <arthur@mondon.pro>
2024-01-29 21:27:25 +0100

    avoid hands glitch by using lerp, and avoid destroy hands at each update

--- UnityProject_save_the_turtles/Assets/Scripts/CalibrationManager.cs
+++ UnityProject_save_the_turtles/Assets/Scripts/CalibrationManager.cs
@@ -13,9 +13,10 @@ using System.Collections.Concurrent;
 
 public class CalibrationManager : MonoBehaviour
 {
-    public GameObject wristPrefab; // Préfabriqué pour les poignets
+    public GameObject wristPrefab;
     public GameObject planeGameObject;
-    private List<GameObject> instantiatedWristPrefabs = new List<GameObject>(); // Liste pour stocker les instances
+    private GameObject[] wristInstances;
+    private int maxHands = 2; // Nombre maximum de mains à gérer
     public static CalibrationManager Instance { get; private set; }
 
     void Awake()
@@ -23,6 +24,12 @@ public class CalibrationManager : MonoBehaviour
         if (Instance == null)
         {
             Instance = this;
+            wristInstances = new GameObject[maxHands];
+            for (int i = 0; i < maxHands; i++)
+            {
+                wristInstances[i] = Instantiate(wristPrefab, Vector3.zero, Quaternion.identity);
+                wristInstances[i].SetActive(false);
+            }
         }
         else
         {
@@ -39,27 +46,41 @@ public class CalibrationManager : MonoBehaviour
         Debug.Log("Largeur du Plane: " + planeWidth);
         Debug.Log("Hauteur du Plane: " + planeHeight);
         UdpConnectionManager.Instance.SendData("start_calibration");
-
     }
-    
+
+    private void ResetCollider(GameObject wristInstance)
+    {
+        BoxCollider collider = wristInstance.GetComponent<BoxCollider>();
+        if (collider != null)
+        {
+            collider.enabled = false;
+            collider.enabled = true;
+        }
+    }
+
     public void UpdateWristPositions(JSONArray handPositions)
     {
-        ClearWristInstances();
+        int index = 0;
         foreach (JSONNode pos in handPositions)
         {
-            Vector3 worldPos = ConvertToPlanePosition(pos[0].AsFloat, pos[1].AsFloat);
-            GameObject wristInstance = Instantiate(wristPrefab, worldPos, Quaternion.identity);
-            instantiatedWristPrefabs.Add(wristInstance);
+            if (index >= maxHands) break; // Ne pas dépasser le nombre max de mains gérées
+
+            Vector3 targetPos = ConvertToPlanePosition(pos[0].AsFloat, pos[1].AsFloat);
+            if (!wristInstances[index].activeSelf) wristInstances[index].SetActive(true);
+
+            // Interpolation linéaire pour lisser le mouvement
+            wristInstances[index].transform.position = Vector3.Lerp(wristInstances[index].transform.position, targetPos, Time.deltaTime * 70);
+
+            ResetCollider(wristInstances[index]);
+
+            index++;
         }
-    }
-    
-    private void ClearWristInstances()
-    {
-        foreach (GameObject wristInstance in instantiatedWristPrefabs)
+
+        // Désactiver les GameObjects inutilisés
+        for (int i = index; i < maxHands; i++)
         {
-            Destroy(wristInstance); 
+            wristInstances[i].SetActive(false);
         }
-        instantiatedWristPrefabs.Clear(); 
     }
 
     private Vector3 ConvertToPlanePosition(float x, float y)
@@ -67,15 +88,14 @@ public class CalibrationManager : MonoBehaviour
         Mesh mesh = planeGameObject.GetComponent<MeshFilter>().mesh;
         float planeWidth = mesh.bounds.size.x * planeGameObject.transform.localScale.x;
         float planeHeight = mesh.bounds.size.z * planeGameObject.transform.localScale.z;
-        
-        y = 1 - y;
-        
+
+        y = 1 - y; // Inverser l'axe Y
+
         // Conversion des coordonnées
         float posX = (x * planeWidth) - (planeWidth / 2f);
         float posY = (y * planeHeight) - (planeHeight / 2f);
 
-        return new Vector3(posX, 0.5f, posY);
+        return new Vector3(posX, 0.5f, posY); // Position ajustée sur le plan
     }
-
 }
 
